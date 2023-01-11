0fac0be5f30fa22063b119fbe8d2f6dd0df44222
Fraz Aslam <faslam7646@gmail.com>
2023-01-10 23:55:54 -0500

    created player function and board events

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const Gameboard = (() => {
     const gameboard = ["X", "O", "X", "O"];
 
     const board = document.querySelector(".main");
-    
     const renderGameboard = () => {
         
     };
@@ -13,24 +12,58 @@ const Gameboard = (() => {
             board.append(i);
         })
     }
+
+    const tiles = (()=>{
+        const grid = document.querySelectorAll(".button")
+        let currentPlayer = true;
+        
+        grid.forEach((tile)=>{
+            tile.addEventListener("click", ()=>{
+                if ((currentPlayer === true) && (tile.innerHTML == "")) {
+                    tile.innerHTML= (gameControl.player1.symbol);
+                    currentPlayer = false;
+                }
+
+                else if ((currentPlayer === false) && (tile.innerHTML == "")){
+                    tile.innerHTML = (gameControl.player2.symbol);
+                    currentPlayer = true;
+                }
+                
+            })
+         
+        })
+    })();
+
+
     return {
         gameboard,
         renderGameboard,
-        printArray
-        
+        printArray,
     }
 
 })();
 
 Gameboard.printArray();
 
-// Factory Function for Players
-const Player = (name, symbol) => {
 
+// Factory Function for Players
+const Player = (symbol) => {
+    
+    return {symbol};
 };
 
 // Module - To Control Game
 const gameControl = (() => {
+    let player1 =  Player("X");
+    let player2 = Player("O");
 
+
+
+    return {
+        player1,
+        player2
+    }
 })();
+
+
 
