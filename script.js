94f1df0d1dbb6f955ae8f4b4954dc5b662123ed1
Fraz Aslam <faslam7646@gmail.com>
2023-01-17 22:05:15 -0500

    finished project, need to add AI if wanted

--- script.js
+++ script.js
@@ -5,7 +5,10 @@ let wrapper = document.querySelector(".wrapper");
 let form = document.querySelector(".form");
 let board = document.querySelector(".gameBoard");
 let mainbox = document.querySelector(".mainBox");
+let reset = document.querySelector(".refresh");
 
+let player1Count = 0;
+let player2Count = 0;
 
 
 const Gameboard = (() => {
@@ -55,10 +58,6 @@ const gameControl = (() => {
     return {
         player1,
         player2,
-
-        
-        
-        
     }
 })();
 
@@ -66,18 +65,21 @@ const gameControl = (() => {
 const tiles = (() => {
     const grid = document.querySelectorAll(".button");
     const playerTurn = document.querySelector(".playerTurn");
+    const box1 = document.querySelector(".playerBox1");
+    const box2 = document.querySelector(".playerBox2");
     const btnsArr = Array.from(grid);
-
-   
-    
-    
     let currentPlayer = true;
+    let gameCount = 0;
+
+    box1.classList.add("playerBoxOne")
     
+
     for (let i = 0; i < btnsArr.length; i++){
         btnsArr[i].addEventListener("click", ()=>{    
         if (gameStart === true){
             if((currentPlayer === true) &&  (Gameboard.gameboard[i] === "")){
                // playerTurn.textContent = `IT IS PLAYER 2 TURN`
+               
                 Gameboard.gameboard[i] = gameControl.player1.symbol;
                 btnsArr[i].classList.add("totodile");
                 const symbolX = document.createElement("img");
@@ -86,6 +88,8 @@ const tiles = (() => {
                 //btnsArr[i].textContent = Gameboard.gameboard[i];
                 btnsArr[i].appendChild(symbolX);
                 wonGame();
+                box1.classList.remove("playerBoxOne");
+                box2.classList.add("playerBoxTwo");
                 currentPlayer = false;
             }
             else if((currentPlayer === false) && (Gameboard.gameboard[i] === "")){
@@ -98,12 +102,39 @@ const tiles = (() => {
                 //btnsArr[i].textContent = Gameboard.gameboard[i];
                 btnsArr[i].appendChild(symbolO);
                 wonGame();
+                box2.classList.remove("playerBoxTwo");
+                box1.classList.add("playerBoxOne")
                 currentPlayer = true;
             }
             
         }
+        
         })
     }
+
+    
+            reset.addEventListener("click", ()=>{
+            Gameboard.gameboard = [
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        "",
+                        ""
+                    ];
+            for(let i = 0; i < btnsArr.length; i++){
+                btnsArr[i].innerHTML = "";
+                btnsArr[i].classList.remove("gengar")
+                btnsArr[i].classList.remove("totodile")
+            }
+            
+            gameStart = true;
+    
+})
+    
 
 })();
 
@@ -120,8 +151,11 @@ function wonGame() {
         [0,4,8],
         [2,4,6]
     ];
+    
     let gameWon = false;
     let a;
+   
+
     for (let i = 0; i < winConditions.length; i++){
         const condition = winConditions[i];
         a = Gameboard.gameboard[condition[0]];
@@ -144,19 +178,62 @@ function wonGame() {
 
     if (gameWon && a === "X"){
         console.log(`${gameControl.player1.symbol} has won`);
+        player1Count++;
         let tree = document.querySelector(".playerInfo1");
         let winnerDiv = document.createElement("div")
-        winnerDiv.textContent = "WINNER!";
-        tree.appendChild(winnerDiv);
+        winnerDiv.classList.add("winnerText");
+        winnerDiv.textContent = "WINNER";
+        let winnerBox = document.querySelector(".winnerBox");
+        winnerBox.appendChild(winnerDiv)
+        
+        let pokeball = document.createElement("img");
+        pokeball.classList.add("pokeball");
+        pokeball.src="pokeball.png";
+        let pokeBallBox = document.querySelector(".winnerPokeBalls");
+        pokeBallBox.appendChild(pokeball);
+        reset.addEventListener("click", ()=>{
+            winnerDiv.textContent = "";
+            
+        });
+        
+        if(player1Count === 3){
+            console.log("YOU ARE THE WORLD CHAMP!!!")
+            document.querySelector(".gameboardMain").remove();
+            reset.remove();
+            winnerDiv.textContent = "WORLD CHAMPION";
+            document.querySelector("playerBox2").classList.remove("playerBoxTwo")
+        }
         gameStart = false;
+
+        
     }
     else if (gameWon && a === "O"){
         console.log(`${gameControl.player2.symbol} has won`);
+        player2Count++;
         let tree = document.querySelector(".playerInfo2");
         let winnerDiv = document.createElement("div")
-        winnerDiv.textContent = "WINNER!";
-        tree.appendChild(winnerDiv);
+        winnerDiv.classList.add("winnerText");
+        winnerDiv.textContent = "WINNER";
+        let winnerBox = document.querySelector(".winnerBox2");
+        winnerBox.appendChild(winnerDiv);
+        let pokeball = document.createElement("img");
+        pokeball.classList.add("pokeball");
+        pokeball.src="pokeball.png";
+        let pokeBallBox = document.querySelector(".winnerPokeBalls2");
+        pokeBallBox.appendChild(pokeball);
+        reset.addEventListener("click", ()=>{
+            winnerDiv.textContent = "";
+        })
+        
+        if(player2Count === 3){
+            console.log("YOU ARE THE WORLD CHAMP!")
+            document.querySelector(".gameboardMain").remove();
+            reset.remove();
+            winnerDiv.textContent = "WORLD CHAMPION";
+            document.querySelector("playerBox1").classList.remove("playerBoxOne")
+        }
         gameStart = false;
+       
     }
 
     if (!Gameboard.gameboard.includes("") && gameWon === false){
@@ -164,15 +241,22 @@ function wonGame() {
         let tree2 = document.querySelector(".playerInfo2")
         let tieDiv = document.createElement("div");
         let tieDiv2 = document.createElement("div");
-        tieDiv.textContent = "TIE!";
-        tieDiv2.textContent = "TIE!"
+        tieDiv.classList.add("winnerText");
+        tieDiv2.classList.add("winnerText");
+        tieDiv.textContent = "TIE";
+        tieDiv2.textContent = "TIE";
         tree.appendChild(tieDiv);
         tree2.appendChild(tieDiv2);
-        console.log("TIE");
+        reset.addEventListener("click", ()=>{
+            tieDiv.textContent = "";
+            tieDiv2.textContent = "";
+        })
         gameStart = false;
 
 
     }
+
+
 }
 
 
